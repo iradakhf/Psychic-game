a176b9eb5f14928a430c790d6d3ac7d77a5dbcbc
iradakhf <iradakhf@code.edu.az>
2023-01-31 14:33:09 +0400

    added all

---
+++ app.js
@@ -0,0 +1,54 @@
+const letters = ['a','b','c','d','e','f','g','h','i',
+  'j','k','l','m','n','o','p', 'q','r',
+  's','t','u','v','w','x','y','z'];
+
+var lettersGuessed = [];
+var guessesLeft = 10;
+var wins = 0;
+var losses = 0;
+
+function generateRandomAnswer(){
+    let randomLetterIndex = Math.floor(Math.random() * letters.length);
+    return letters[randomLetterIndex];
+  }
+  
+var computerGuess = generateRandomAnswer();
+function start(){
+  guessesLeft=10; 
+}
+window.addEventListener("keyup", function(e){
+  
+  if(letters.includes(e.key)){
+    var userguess = document.getElementById("usersGuess").innerHTML;
+    if(userguess.includes(e.key)){
+      this.alert(`${e.key} is already guessed`)
+    }
+    else{
+       userguess = document.getElementById("usersGuess").innerHTML +=e.key + ",";
+  
+       if(e.key==computerGuess){
+        wins+=1;
+        document.getElementById("Wins").innerText = wins;
+        this.alert("you won!")
+        start();
+       }
+       else{
+         if(guessesLeft>0){
+           guessesLeft-=1;
+           document.getElementById("guessesLeft").innerText= guessesLeft;
+         }
+         else{
+          losses +=1
+           document.getElementById("Losses").innerText = losses;
+          this.alert("no guesses left")
+          start();
+         }
+       }
+    }
+
+  }
+  else{
+    alert("please enter a letter")
+  }
+})
+
